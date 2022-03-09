666f41e893b6237df19fdc5497d967e45f9907cd
Ilya Kupriyanov <ikupdev@gmail.com>
2022-03-09 22:23:50 +0300

    refactoring

--- iron-inner-starter/src/main/java/ru/kuper/raven/ProfileDetectorEPP.java
+++ iron-inner-starter/src/main/java/ru/kuper/raven/ProfileDetectorEPP.java
@@ -7,16 +7,17 @@ import org.springframework.core.env.ConfigurableEnvironment;
 /**
  * @author Ilya V. Kupriyanov
  * @version 09.03.2022
+ * //Выполняется перед инициализацией бинов (до ApplicationContextInitializer-ов), после настройки окружения
  */
 public class ProfileDetectorEPP implements EnvironmentPostProcessor {
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-        if (environment.getActiveProfiles().length == 0 && getTemperature() < 272) {
-            environment.setActiveProfiles("WinterThere");
+        if (environment.getActiveProfiles().length == 0) {
+            String profile = (getTemperature() < 272) ? "WinterThere" : "WinterIsComing";
+            environment.setActiveProfiles(profile);
         } else {
-            environment.setActiveProfiles("WinterIsComing");
+            System.out.println("We do not need to set Profile!");
         }
-
     }
 
     private int getTemperature() {
